e026ac33020d90bdfc2eb1472f2613212324e7c0
DickLan <pokemaster556610@gmail.com>
2023-12-18 17:30:16 +0800

    load local boostrap 4.1.1 and jquery and popper

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,17 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
-    <router-view/>
+    <router-view />
   </div>
 </template>
+
+<script>
+import "bootstrap"; // 表示要載入 boostrap 的 js 因為在 node_modules/bootstrap/package.json 的 main 中 有指定預設路徑為 "main": "dist/js/bootstrap.js"
+import "bootstrap/dist/css/bootstrap.css";
+export default {
+  name: "App",
+};
+</script>
 
 <style>
 #app {
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFound from '../views/NotFound.vue'
 
 Vue.use(VueRouter)
 
@@ -8,6 +9,7 @@ const routes = [
   {
     path: '/',
     name: 'home',
+    // 每一次進入routes 都會載入 component HomeView
     component: HomeView
   },
   {
@@ -16,8 +18,14 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
+    // 只有進入 /about 時，才會載入 AboutView
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }, {
+    path: '*', // * 表示任意路徑
+    name: 'not-found',
+    component: NotFound
   }
+
 ]
 
 const router = new VueRouter({
