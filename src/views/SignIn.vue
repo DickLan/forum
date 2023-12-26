b20ae0d9e5683dd3f7b8fc9442f5ba48702063eb
DickLan <pokemaster556610@gmail.com>
2023-12-26 17:16:45 +0800

    transfer promise to async

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -68,43 +68,43 @@ export default {
     };
   },
   methods: {
-    handlerSubmit(e) {
-      // 在前端先做檢測 如果帳密任一為空 則使用 toast and return
-      if (!this.password || !this.email) {
-        Toast.fire({
-          icon: "warning",
-          title: "請填入 email 和 password",
-        });
-      }
-      this.isProcessing = true;
-      authorizationAPI
-        .signIn({
-          email: this.email,
-          password: this.password,
-        })
-        .then((response) => {
-          console.log("response=", response);
-          const { data } = response;
-          if (data.status === "error") {
-            throw new Error(data.message);
-          }
-          // localStorage 是瀏覽器給每個網頁的儲存空間
-          // 可以把想儲存的資料放在瀏覽器內
-          // 在使用者清除 localStorage 之前，就算關閉網頁，這localStorage仍然存在
-          localStorage.setItem("token", data.token);
-          // vue router 提供的轉址功能
-          this.$router.push("/restaurants");
-        })
-        .catch((err) => {
-          this.isProcessing = false;
-          this.password = "";
+    async handlerSubmit(e) {
+      try {
+        // 在前端先做檢測 如果帳密任一為空 則使用 toast and return
+        if (!this.password || !this.email) {
           Toast.fire({
             icon: "warning",
-            title: "請確認您輸入了正確的帳號密碼",
+            title: "請填入 email 和 password",
           });
-          console.log("err=", err);
+        }
+        this.isProcessing = true;
+
+        const response = await authorizationAPI.signIn({
+          email: this.email,
+          password: this.password,
         });
+        console.log("response=", response);
+        const { data } = response;
+        if (data.status === "error") {
+          throw new Error(data.message);
+        }
+        // localStorage 是瀏覽器給每個網頁的儲存空間
+        // 可以把想儲存的資料放在瀏覽器內
+        // 在使用者清除 localStorage 之前，就算關閉網頁，這localStorage仍然存在
+        localStorage.setItem("token", data.token);
+        // vue router 提供的轉址功能
+        this.$router.push("/restaurants");
+      } catch (error) {
+        this.isProcessing = false;
+        this.password = "";
+        Toast.fire({
+          icon: "warning",
+          title: "請確認您輸入了正確的帳號密碼",
+        });
+        console.log("error=", error);
+      }
     },
+    handleSub() {},
   },
 };
 </script>
